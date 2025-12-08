13105ed628ec98edd24370e43f5eb20ff42041c2
Monsieur-Code1 <abdelrhmanelsyed7@gmail.com>
2025-12-08 20:23:28 +0300

    edit

--- src/feature/pageAboutUs/About.jsx
+++ src/feature/pageAboutUs/About.jsx
@@ -1,9 +1,11 @@
 // import './About.css';
-import style from "./About.module.css"
-import style1 from "../style/ButtonNow.module.css"
-import { useData } from "../../context/Context";
+import { useState } from 'react';
+import { useData } from '../../context/Context';
+import style1 from '../style/ButtonNow.module.css';
+import style from './About.module.css';
 export default function About() {
   const { aboutRef } = useData();
+  const [ShowMore, setShowMore] = useState(false);
   return (
     <section ref={aboutRef} className={style.containerPage}>
       <div className={style.containerTitle}>
@@ -18,16 +20,33 @@ export default function About() {
         <div className={style.containerText}>
           <h4 className={style.h4}>We Are Feane</h4>
           <p className={style.paragraph}>
-            There are many variations of passages of Lorem Ipsum available, but
+            {ShowMore
+              ? `There are many variations of passages of Lorem Ipsum available, but
             the majority have suffered alteration in some form, by injected
             humour, or randomised words which don't look even slightly
             believable. If you are going to use a passage of Lorem Ipsum, you
             need to be sure there isn't anything embarrassing hidden in the
-            middle of text.
+            middle of text.There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text.`
+              : `There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text.`}
           </p>
 
           <div className={style.containerBtnOrder}>
-            <button className={style1.btnOrderNow}>READ MORE</button>
+            <button
+              onClick={() => setShowMore((more) => !more)}
+              className={style1.btnOrderNow}
+            >
+              {ShowMore ? 'READ LEES' : 'READ MORE'}
+            </button>
 
             {/* <button className={style.btnOrderNow}> MORE</button> */}
             <img
